4d8ea654c1b06711a1558b7d840daff068451417
rmacaw <mzmacaw@gmail.com>
2020-11-30 14:10:33 -0700

    Initial commit

---
+++ E6PigLatinSentence.py
@@ -0,0 +1,8 @@
+# Translate a series of English words into Pig Latin
+# Write a function called pl_sentence that takes a string containing
+# several words separated by spaces. Don't have to handle 'real sentences,
+# punctuation or capitals. Example: pl_sentence('this is a test translation')
+# should output: histay isway away esttay ranslationtay
+
+def pl_sentence():
+    pass
