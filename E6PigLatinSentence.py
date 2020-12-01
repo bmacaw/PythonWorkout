6bc8b4046ad8e22e1a4673e946a1bf95e49158c3
rmacaw <mzmacaw@gmail.com>
2020-12-01 14:14:56 -0700

    Initial commit Complete pl_sentence()

--- E6PigLatinSentence.py
+++ E6PigLatinSentence.py
@@ -4,5 +4,16 @@
 # punctuation or capitals. Example: pl_sentence('this is a test translation')
 # should output: histay isway away esttay ranslationtay
 
-def pl_sentence():
-    pass
+def pl_sentence(words_string):
+    word_list = []
+    vowels = 'aeiou'
+    for word in words_string.split():
+        if word[0] in vowels:
+            word_list.append(f'{word}way')
+        else:
+            word_list.append(f'{word[1:]}ay')
+
+    return ' '.join(word_list)
+
+
+print(pl_sentence('this is a test translation'))
