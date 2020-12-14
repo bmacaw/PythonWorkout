fdbde613507ffd4505dc15a7b28e076d10800047
rmacaw <mzmacaw@gmail.com>
2020-12-14 14:47:29 -0700

    Complete last_word_alphabetically()

--- E8SortingAString.py
+++ E8SortingAString.py
@@ -17,17 +17,20 @@ str_3 = 'Cba'
 list_o_strings = [str_1, str_2, str_3]
 for string in list_o_strings:
     print(strsort(string))
+# Output: ABCabc
+#         abc
+#         Cab
 
 
 # Beyond the exercise
 # Strings are sequences and can be put wherever other sequences (lists and tuples) can be used.
 # sorted() returns a list. In order to get a string, we need to turn the resulting list back
 # into a string (use str.()). str.split() and str.join() are two helpful methods to really know.
-# Consider variations using str.split(), str.join() and sorted:
+# Consider variations using str.split(), str.join() and sorted (note-my solutions are all from
+# author's solutions...yeah, what a cop-out, right?
 
-# TODO:Given the string "Tom Dick Harry," break it into individual words, and then sort those words
+# Given the string "Tom Dick Harry," break it into individual words, and then sort those words
 # alphabetically. Once they're sorted, print them with commas (,) between the names.
-# From author's solution:
 
 
 def sort_words(some_string):
@@ -38,10 +41,29 @@ def sort_words(some_string):
     return ', '.join(one_word for one_word in sorted(some_string.split()))
 
 
-print(sort_words('Tom Dick Harry'))
+print(sort_words('Tom Dick Harry')) # Output: Dick, Harry, Tom
 
 
-# TODO:Which is the last word alphabetically, in a text file?
+# Which is the last word alphabetically, in a text file?
+
+
+def last_word_alphabetically(filename):
+    """Given the name of a text file, return the word that comes last (alphabetically)
+    in that file.
+    """
+
+    output = ''
+    for one_line in open(filename):
+        for one_word in one_line.split():
+            if not one_word.isalpha():
+                continue
+            if one_word > output:
+                output = one_word
+    return output
+
+
+print(last_word_alphabetically('wordperlinetest.txt')) # Output: your
+
 
 
 # TODO:Which is the longest word in a text file?
