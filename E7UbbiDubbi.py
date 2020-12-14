72ef77ffb22a05c1a2e68e98139fa33903f54cd6
rmacaw <mzmacaw@gmail.com>
2020-12-14 12:52:10 -0700

    Handle capitalized words

--- E7UbbiDubbi.py
+++ E7UbbiDubbi.py
@@ -1,7 +1,7 @@
 # Write a function called ubbi_dubbi that takes a single word (string) as an argument
 # and returns a string, the word's translation into Ubbi Dubbi. Example: if the
 # function is called with octopus, the function will return the string uboctubopubus.
-
+import string
 
 def ubbi_dubbi(any_word):
     ud_translation = []
@@ -24,7 +24,7 @@ for word in word_list:
 # Python has str.replace() and string.translate(), but sometimes have to iterate
 # over a string and append modified version of what we want.
 
-# TODO: Handle capitalized words
+# Handle capitalized words
 def ubbi_dubbi(any_word):
     ud_translation = []
     for one_letter in any_word:
@@ -32,4 +32,17 @@ def ubbi_dubbi(any_word):
             ud_translation.append(f'ub{one_letter}')
         else:
             ud_translation.append(one_letter)
+
+    # Note: I took this part from the solution. It's slick imo
+    if any_word[0] in string.ascii_uppercase:
+        ud_translation[0] = ud_translation[0].capitalize()
+
     return ''.join(ud_translation)
+
+
+word_1 = 'Rebecca'
+word_2 = 'banana'
+word_3 = 'Finn'
+word_list = [word_1, word_2, word_3]
+for word in word_list:
+    print(ubbi_dubbi(word))   # Expect and get Rubebubeccuba bubanubanuba Fubinn
