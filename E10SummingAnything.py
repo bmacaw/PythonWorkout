d02c368eac06799aea85da9eeb384d7b2b208040
rmacaw <mzmacaw@gmail.com>
2020-12-28 11:55:03 -0700

    Complete and test my_sum_bigger_than() Complete and test sum_numeric()

--- E10SummingAnything.py
+++ E10SummingAnything.py
@@ -33,7 +33,26 @@ print(mySum([10, 20, 30], [40, 50, 60], [70, 80]))  # => [10, 20, 30, 40, 50, 60
 # my_sum_bigger_than(10, 5, 20, 30, 6) would return 50 because 5 and 6 aren't
 # greater than 10. The function should work with any type and assumes all
 # args are same type.
+def my_sum_bigger_than(threshold_value, *items):
+    """Sum items, which should be of the same type.
+    Ignore any below the value of threshold.
+    The arguments should handle the + operator.
+    If passed no arguments, then return an empty tuple.
+    """
 
+    if not items:
+        return items
+
+    result = 0
+
+    for item in items:
+        if item > threshold_value:
+             result += item
+
+    return result
+
+
+print(my_sum_bigger_than(10, 5, 20, 30, 6))  # => 50
 
 
 # Write a function, sum_numeric() that takes any number of arguments. If the
@@ -41,6 +60,23 @@ print(mySum([10, 20, 30], [40, 50, 60], [70, 80]))  # => [10, 20, 30, 40, 50, 60
 # total. Arguments that can't be handled as integers should be ignored. The
 # result is the sum of the numbers. Thus sum_numeric(10, 20, 'a', '30', 'bcd')
 # would return 60 (the string '30' is converted into the int 30)
+def sum_numeric(*args):
+    """Sum all argument items, assuming that they
+    are integers or can be turned into integers.
+    """
+
+    total = 0
+
+    for item in args:
+        try:
+            total += int(item)
+        except ValueError:
+            pass
+    return total
+
+
+print(sum_numeric(10, 20, 'a', '30', 'bcd'))  # => 60
+
 
 # Write a function that takes a list of dicts and returns a single dict that
 # combines all of the keys and values. If a dey appears in more than one
