7dba8319ea19c9bec9c7371f6e349ea2c93106a8
rmacaw <mzmacaw@gmail.com>
2020-12-28 11:42:13 -0700

    Test mySum()

--- E10SummingAnything.py
+++ E10SummingAnything.py
@@ -7,6 +7,7 @@
 # support for dicts--so note to self: try this! :)
 
 
+# Author's solution
 def mySum(*list_items):
     if not list_items:
         return list_items
@@ -16,7 +17,33 @@ def mySum(*list_items):
     for list_item in list_items[1:]:
         result += list_item
 
-        return result
+    return result
 
 
-print(mySum(10, 20, 30, 40))
+print(mySum())  # => ()
+print(mySum(10, 20, 30, 40))  # => 100
+print(mySum('a', 'b', 'c', 'd'))  # => abcd
+print(mySum([10, 20, 30], [40, 50, 60], [70, 80]))  # => [10, 20, 30, 40, 50, 60, 70, 80]
+
+
+# Beyond the Exercise
+# Write a function my_sum_bigger_than() that works the same as mySum(),
+# except that it works for an argument that precedes *args. That argument
+# indicates the threshold for including an argument in the sum. Thus, calling
+# my_sum_bigger_than(10, 5, 20, 30, 6) would return 50 because 5 and 6 aren't
+# greater than 10. The function should work with any type and assumes all
+# args are same type.
+
+
+
+# Write a function, sum_numeric() that takes any number of arguments. If the
+# argument is or can be turned into an integer, then it should be added to the
+# total. Arguments that can't be handled as integers should be ignored. The
+# result is the sum of the numbers. Thus sum_numeric(10, 20, 'a', '30', 'bcd')
+# would return 60 (the string '30' is converted into the int 30)
+
+# Write a function that takes a list of dicts and returns a single dict that
+# combines all of the keys and values. If a dey appears in more than one
+# argument, the value should be a list containing all of the values from the
+# arguments.
+
